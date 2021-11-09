16760bab58420f5990b6d2cb24357dd075b6374f
surprising123 <839841127@qq.com>
2021-11-09 14:56:55 +0000

    v11

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,17 +10,7 @@ public class GitTest {
 		System.out.println("hhahah");
 		System.out.println("hhahah22222");
 		System.out.println("hhahah22222333333333333");
-		System.out.println("hhahah22222333333333333hot-fix");
-		System.out.println("mmmmmmmmmm");
-		System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhh");
-		System.out.println("2m2mm2m2m2m2m");
-		System.out.println("h2h2h2h");
-		System.out.println("h2h2h2h333333333333333333333");
-		System.out.println("9999999999999999999999999999999");
-		System.out.println("yyyyyyyyyy");
-		System.out.println("ddddddddddddddd");
-
-		System.out.println("yffffffyyyyy");
+		
 
 	}
 }
